0950f3764923416cf88a8fc4deff810ff37a17f0
aksingh0108 <ankitsingh.shiva108@gmail.com>
2024-08-28 14:45:48 +0530

    Auto-generated commit

---
+++ server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-EHRSPVAC.mjs";import"./chunk-4E6ATXKN.mjs";import"./chunk-UYFIRDFP.mjs";import"./chunk-YJRZK3GP.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
